046a6f3d15821da1986e0e4836587b9bcd520425
Md. Armanul Islam <118989268+arman1342002@users.noreply.github.com>
2025-01-12 13:12:41 +0600

    Update login_val.php

--- Final Lab Exam/controller/login_val.php
+++ Final Lab Exam/controller/login_val.php
@@ -1,15 +1,16 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
 
-    if (empty($username) || empty($password)) {
-        die('Username and password are required.');
+            if (empty($username) || empty($password)) {
+                die('Username and password are required.');
+            }
+            else{
+                $result = registerEmployee($name, $contact, $username, $password);
+            }
+
+
     }
-    else{
-        $result = registerEmployee($name, $contact, $username, $password);
-    }
-
-
-}
 ?>
