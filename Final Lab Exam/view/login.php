27f3158ed54e56d3a12c2b2b4f6e9bf81a2df74d
Md. Armanul Islam <118989268+arman1342002@users.noreply.github.com>
2025-01-12 12:55:41 +0600

    Add files via upload

---
+++ Final Lab Exam/view/login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Shop Management System</title>
+</head>
+
+<body>
+    <h3>Employee Login</h3>
+        <label>Username: </label><br>
+        <input type="text" id="username"><br>
+        <label>Password: </label><br>
+        <input type="password" id="password"><br>
+        <button type="submit">Login</button>
+
+</body>
---
+++ Final Lab Exam/view/registration.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <title>Shop Management System</title>
+</head>
+
+<body>
+    <h3>Employee Registration</h3>
+    <form action="../controller/reg_val.php" method="POST">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="contact">Contact No:</label><br>
+        <input type="text" id="contact" name="contact" required><br><br>
+
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <button type="submit">Register</button><br><br>
+        <label for="name">Already have account?</label>
+        <a href="login.php">Login</a>
+    </form>
+
+
+    <script>
+        function displayMessage(msg, color = 'green')
+        {
+            message = document.getElementById('message');
+            message.textContent = msg;
+            messag.style.color = color;
+            setTimeout
+        }
+
+        function registerEmployee()
+        {
+
+        }
+    </script>
+
+</body>
+</html>
